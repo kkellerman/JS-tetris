4b85fd30e1441f4a05e8f1479d64bec5da1c330c
Keith K <k.kellerman@dnp.imgcomm.com>
2020-05-15 08:38:51 -0400

    started css and app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.grid div'))
+const ScoreDisplay = document.querySelector('#score')
+const StartBin = document.querySelector('#score-button')
 
-
+const width = 10
 
 
 })
