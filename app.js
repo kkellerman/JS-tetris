af8e481f30408aff0c29959c19ea999a0322eb01
Keith K <k.kellerman@dnp.imgcomm.com>
2020-05-17 13:03:17 -0400

    added move left and right functions

--- app.js
+++ app.js
@@ -117,6 +117,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+
+  //move the tetromino left, unless is at the edge or there is a blockage
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    if(!isAtLeftEdge) currentPosition -=1
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition +=1
+    }
+    draw()
+  }
+
+  //move the tetromino right, unless is at the edge or there is a blockage
+  function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+    if(!isAtRightEdge) currentPosition +=1
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1
+    }
+    draw()
+  }
  
 
 })
